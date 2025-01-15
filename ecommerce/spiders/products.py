0fd74ade80c3c76537b3534e1a0b53948924bfe9
Bondzik-S <bondziks@gmail.com>
2025-01-15 17:14:39 +0200

    fix flake

--- ecommerce/spiders/products.py
+++ ecommerce/spiders/products.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import scrapy
 from scrapy.http import Response
 
@@ -9,51 +7,17 @@ class ProductsSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    # def parse(self, response: Response, **kwargs):
-    #     for product in response.css(".product_pod"):
-    #         yield {
-    #             # price
-    #             # amount_in_stock
-    #             # rating
-    #             # category
-    #             # description
-    #             # upc
-    #             "title": product.css("a::attr(title)").get(),
-    #             "price": float(
-    #                 product.css(".price_color::text").get().replace("£", "")
-    #             ),
-    #             # "amount_in_stock": product.css("").get(),
-    #             "rating": product.css("p.star-rating::attr(class)").get().split()[1],
-    #             # "category": product.css("").get(),
-    #             # "description": product.css("").get(),
-    #             # "upc": product.css("").get()
-    #         }
-    #
-    #         detail_url = response.urljoin(product.css("h3 a::attr(href)").get())
-    #         yield response.follow(
-    #             detail_url,
-    #             callback=self.parse_product_details
-    #         )
-    #     next_page = response.css(".next a::attr(href)").get()
-    #     if next_page is not None:
-    #         yield response.follow(next_page, callback=self.parse)
-    #
-    #
-    # def parse_product_details(self, response: Response) -> None:
-    #     yield {
-    #         "amount_in_stock": int(response.css("th:contains('Availability') + td::text").get().split("(")[-1].split(" ")[0]),
-    #         "category": response.css(".breadcrumb li:nth-child(3) a::text").get(),
-    #         "description": response.css("#product_description + p::text").get(),
-    #         "upc": response.css("th:contains('UPC') + td::text").get()
-    #     }
-
-    def parse(self, response: Response, **kwargs):
+    def parse(self, response: Response, **kwargs) -> None:
         for product in response.css(".product_pod"):
             title = product.css("a::attr(title)").get()
-            price = float(product.css(".price_color::text").get().replace("£", ""))
+            price = float(
+                product.css(".price_color::text").get().replace("£", "")
+            )
             rating = product.css("p.star-rating::attr(class)").get().split()[1]
 
-            detail_url = response.urljoin(product.css("h3 a::attr(href)").get())
+            detail_url = response.urljoin(
+                product.css("h3 a::attr(href)").get()
+            )
             yield response.follow(
                 detail_url,
                 callback=self.parse_product_details,
@@ -68,14 +32,20 @@ class ProductsSpider(scrapy.Spider):
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
-    def parse_product_details(self, response: Response):
+    def parse_product_details(self, response: Response) -> None:
         data = response.meta
         data.update({
             "amount_in_stock": int(
-                response.css("th:contains('Availability') + td::text").get().split("(")[-1].split(" ")[0]
+                response.css(
+                    "th:contains('Availability') + td::text"
+                ).get().split("(")[-1].split(" ")[0]
             ),
-            "category": response.css(".breadcrumb li:nth-child(3) a::text").get(),
-            "description": response.css("#product_description + p::text").get(),
+            "category": response.css(
+                ".breadcrumb li:nth-child(3) a::text"
+            ).get(),
+            "description": response.css(
+                "#product_description + p::text"
+            ).get(),
             "upc": response.css("th:contains('UPC') + td::text").get()
         })
         yield data
